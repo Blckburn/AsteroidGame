912ac9dc63e0f9444372a591424aa0db11946771
Blckburn <58659133+Blckburn@users.noreply.github.com>
2020-03-03 18:39:28 +0300

    Обстрактный класс графических объектов

--- AsteroidGame/Game.cs
+++ AsteroidGame/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AsteroidGame.VisualObjects;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -47,21 +48,36 @@ namespace AsteroidGame
         public static VisualObject[] __GameObjects;
         public static void Load()
         {
-            __GameObjects = new VisualObject[30];
-            for (var i = 0; i < __GameObjects.Length / 2; i++)
-                __GameObjects[i] = new VisualObject(
-                    new Point(600, i * 20),
-                    new Point(15 - i, 20 - i),
-                    new Size(20, 20));
 
-            for (var i = __GameObjects.Length / 2; i < __GameObjects.Length; i++)
-                __GameObjects[i] = new Star(
-                    new Point(600, i * 20),
-                    new Point(-i, 0),
-                    20);
+            var game_objects = new List<VisualObject>();
+            var rnd = new Random();
 
-            var image = Properties.Resources.Asteroid;
-            var image_object = new ImageObject(new Point(4, 7), new Point(-4, 6), new Size(20, 20), image);
+            const int stars_count = 150;
+            const int star_size = 5;
+            const int star_max_speed = 20;
+            for (var i = 0; i < stars_count; i++)
+                game_objects.Add(new Star(
+                new Point(rnd.Next(0, Width), rnd.Next(0, Height)),
+                new Point(-rnd.Next(0, star_max_speed), 0),
+                star_size));
+
+
+            const int ellipses_count = 20;
+            const int ellipses_size_x = 20;
+            const int ellipses_size_y = 30;
+
+             for (var i = 0; i < ellipses_count; i++)
+                game_objects.Add(new EllipseObject(
+                      new Point(600, i * 20),
+                      new Point(15 - i, 20 - i),
+                      new Size(ellipses_size_x, ellipses_size_y)));
+
+
+
+            //   var image = Properties.Resources.Asteroid;
+            //   var image_object = new ImageObject(new Point(4, 7), new Point(-4, 6), new Size(20, 20), image);
+
+            __GameObjects = game_objects.ToArray();
         }
 
         public static void Draw()
